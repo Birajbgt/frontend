bf6f2d7d0405a92ea29bb45d322765bb6663f8d5
biraj <virajbogati11@gmail.com>
2024-04-28 12:14:46 +0545

    inital

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+
+const Homepage = () => {
+    return (
+        <div>
+            Image Slider
+            Cards
+        </div>
+
+    )
+}
+export default Homepage;
